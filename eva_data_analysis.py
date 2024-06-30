b0b113909f274b4fe7f8fdeb5883f95235298288
kkh451 <kamillaharding@gmail.com>
2024-06-30 15:20:10 +0100

    unfeat: remove inline comments

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -18,19 +18,15 @@ if __name__ == '__main__':
     graph_file = './cumulative_eva_graph.png'
 
     print(f'Reading JSON file {input_file}')
-    # Read the data from a JSON file into a Pandas dataframe
     eva_df = pd.read_json(input_file, convert_dates=['date'])
-    # Clean the data by removing any incomplete rows and sort by date
     eva_df['eva'] = eva_df['eva'].astype(float)
     eva_df.dropna(axis=0, inplace=True)
     eva_df.sort_values('date', inplace=True)
 
     print(f'Saving to CSV file {output_file}')
-    # Save dataframe to CSV file for later analysis
     eva_df.to_csv(output_file, index=False)
 
     print(f'Plotting cumulative spacewalk duration and saving to {graph_file}')
-    # Plot cumulative time spent in space over years
     eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
     eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
     plt.plot(eva_df.date, eva_df.cumulative_time, 'ko-')
