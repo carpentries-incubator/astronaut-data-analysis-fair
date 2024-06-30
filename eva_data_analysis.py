a926f03cc6e0ce826579cc0dad9a2d3764755cfe
kkh451 <kamillaharding@gmail.com>
2024-06-30 15:31:44 +0100

    unfeat: replace informative variable names with uninformative ones

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -7,32 +7,32 @@ import sys
 if __name__ == '__main__':
 
     if len(sys.argv) < 3:
-        input_file = './eva-data.json'
-        output_file = './eva-data.csv'
+        data_f = './eva-data.json'
+        data_t = './eva-data.csv'
         print(f'Using default input and output filenames')
     else:
-        input_file = sys.argv[1]
-        output_file = sys.argv[2]
+        data_f = sys.argv[1]
+        data_t = sys.argv[2]
         print('Using custom input and output filenames')
 
-    graph_file = './cumulative_eva_graph.png'
+    g_file = './cumulative_eva_graph.png'
 
-    print(f'Reading JSON file {input_file}')
-    eva_df = pd.read_json(input_file, convert_dates=['date'])
-    eva_df['eva'] = eva_df['eva'].astype(float)
-    eva_df.dropna(axis=0, inplace=True)
-    eva_df.sort_values('date', inplace=True)
+    print(f'Reading JSON file {data_f}')
+    data = pd.read_json(data_f, convert_dates=['date'])
+    data['eva'] = data['eva'].astype(float)
+    data.dropna(axis=0, inplace=True)
+    data.sort_values('date', inplace=True)
 
-    print(f'Saving to CSV file {output_file}')
-    eva_df.to_csv(output_file, index=False)
+    print(f'Saving to CSV file {data_t}')
+    data.to_csv(data_t, index=False)
 
-    print(f'Plotting cumulative spacewalk duration and saving to {graph_file}')
-    eva_df['duration_hours'] = eva_df['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
-    eva_df['cumulative_time'] = eva_df['duration_hours'].cumsum()
-    plt.plot(eva_df.date, eva_df.cumulative_time, 'ko-')
+    print(f'Plotting cumulative spacewalk duration and saving to {g_file}')
+    data['duration_hours'] = data['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
+    data['cumulative_time'] = data['duration_hours'].cumsum()
+    plt.plot(data.date, data.cumulative_time, 'ko-')
     plt.xlabel('Year')
     plt.ylabel('Total time spent in space to date (hours)')
     plt.tight_layout()
-    plt.savefig(graph_file)
+    plt.savefig(g_file)
     plt.show()
     print("--END--")
