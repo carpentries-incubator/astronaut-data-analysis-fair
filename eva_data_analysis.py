4e3f7310e0db49a999a95a4f15e8fe12810ac8cb
kkh451 <kamillaharding@gmail.com>
2024-06-30 15:11:59 +0100

    unfeat: remove docstrings

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -4,16 +4,6 @@ import sys
 
 
 def read_json_to_dataframe(input_file_):
-    """
-    Read the data from a JSON file into a Pandas dataframe.
-    Clean the data by removing any incomplete rows and sort by date
-
-    Args:
-        input_file_ (str): The path to the JSON file.
-
-    Returns:
-        pd.DataFrame: The loaded dataframe.
-    """
     print(f'Reading JSON file {input_file_}')
     eva_df = pd.read_json(input_file_, convert_dates=['date'])
     eva_df['eva'] = eva_df['eva'].astype(float)
@@ -23,44 +13,16 @@ def read_json_to_dataframe(input_file_):
 
 
 def write_dataframe_to_csv(df_, output_file_):
-    """
-    Write the dataframe to a CSV file.
-
-    Args:
-        df_ (pd.DataFrame): The input dataframe.
-        output_file_ (str): The path to the output CSV file.
-
-    Returns:
-        None
-    """
     print(f'Saving to CSV file {output_file_}')
     df_.to_csv(output_file_, index=False)
 
 def text_to_duration(duration):
-    """
-    Convert a text format duration "HH:MM" to duration in hours
-
-    Args:
-        duration (str): The text format duration
-
-    Returns:
-        float: The duration in hours
-    """
     hours, minutes = duration.split(":")
     duration_hours = int(hours) + int(minutes)/60
     return duration_hours
 
 
 def add_duration_hours_variable(df_):
-    """
-    Add duration in hours (duration_hours) variable to the dataset
-
-    Args:
-        df_ (pd.DataFrame): The input dataframe.
-
-    Returns:
-        pd.DataFrame: A copy of df_ with the new duration_hours variable added
-    """
     df_copy = df_.copy()
     df_copy["duration_hours"] = df_copy["duration"].apply(
         text_to_duration
@@ -69,21 +31,6 @@ def add_duration_hours_variable(df_):
 
 
 def plot_cumulative_time_in_space(df_, graph_file_):
-    """
-    Plot the cumulative time spent in space over years
-
-    Convert the duration column from strings to number of hours
-    Calculate cumulative sum of durations
-    Generate a plot of cumulative time spent in space over years and
-    save it to the specified location
-
-    Args:
-        df_ (pd.DataFrame): The input dataframe.
-        graph_file_ (str): The path to the output graph file.
-
-    Returns:
-        None
-    """
     print(f'Plotting cumulative spacewalk duration and saving to {graph_file_}')
     df_ = add_duration_hours_variable(df_)
     df_['cumulative_time'] = df_['duration_hours'].cumsum()
